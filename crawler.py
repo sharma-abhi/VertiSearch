d5962e411a0afa2419d57e3c14d7509c55c5616d
sharma-abhi <asharma@ccs.neu.edu>
2015-03-14 16:55:35 -0400

    Implementation of text parser modified gitignore to exclude output files

--- crawler.py
+++ crawler.py
@@ -38,7 +38,7 @@ def host_extract(url):
     url = up.urlunparse(new_parsed_url)
     return url
 
-def polite_rules(url):
+def policy_rules(url):
     host_url = host_extract(url)
     rp = robotparser.RobotFileParser()
     rp.set_url(host_url+'/robots.txt')
@@ -47,7 +47,7 @@ def polite_rules(url):
         allowed = True
     else:
         allowed = False
-    print "Url: ", url, "Allowed: ", allowed
+    #print "Url: ", url, "Allowed: ", allowed
     return allowed
 
 '''1518 WORLD WAR 2
@@ -62,89 +62,117 @@ in-links (known) and out-links for the page.'''
 seed_url1 = 'http://en.wikipedia.org/wiki/World_War_II'
 seed_url2 = 'http://en.wikipedia.org/wiki/List_of_World_War_II_battles_involving_the_United_States'
 seed_url3 = 'http://en.wikipedia.org/wiki/Military_history_of_the_United_States_during_World_War_II'
+seed_url4 = 'http://www.history.com/topics/world-war-ii'
 
-front = frontier.FrontierQueue([seed_url1, seed_url2, seed_url3])
+front = frontier.FrontierQueue([seed_url1, seed_url2, seed_url3, seed_url4])
 #front = frontier.FrontierQueue([seed_url1])
 count = 0
 explored = {}
 in_link_dict = {}
+opener = urllib2.build_opener()
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:10.0.1) Gecko/20100101 Firefox/10.0.1',
+}
+
+opener.addheaders = headers.items()
 
 while True:
     #being polite
     time.sleep(1)
-
-    url, in_links = front.pop()
-
+    if front.isEmpty():
+        print "QUEUE EMPTY"
+        break
+    else:
+        url, in_links = front.pop()
+    print count, "Crawl started for URL ", url
     in_link_dict[url] = in_links
 
     if explored.get(url) == None:
         visited = False
     else:
-        print "Url already visited.Skipping... ", url
+        #print "Url already visited.Skipping... ", url
         visited = True
 
-    allowed = polite_rules(url)
+    allowed = policy_rules(url)
 
     if allowed and not visited:
         count += 1
-        req = urllib2.Request(url)
 
-        try: response = urllib2.urlopen(req)
+
+
+        #req = urllib2.Request(url)
+        #try: response = urllib2.urlopen(req)
+        try: response = opener.open(url)
         except urllib2.URLError as e:
-            print e
+            print "Error: ", e," for URL: ", url
+            continue
 
         html = response.read()
+        header_info = response.info().dict['content-type']
+
+        if len(re.findall('text/html',header_info)) == 0:
+            print "Link not text/html.Skipping..."
+            continue
+
         soup = BeautifulSoup(html)
 
+        if soup.title == None:
+            head = "<No Title>"
+        else:
+            head = soup.title.string.encode('ascii','ignore')
+
+        text_list = [''.join(s.findAll(text=True)) for s in soup.find_all('p')]
+        body_text = '.'.join(text_list).encode('ascii','ignore')
+        if soup.find("table") == None:
+            table_text = ''
+        else:
+            table_text = soup.find("table").get_text().encode('ascii','ignore')
+
+        text = body_text + table_text
+        text = text.replace("\n",' ')
+
+        with open("output/"+"VS"+str(count)+".txt",'w') as f:
+            f.write(text)
+        #print "write successful for url", url
 
         links = []
 
         for link in soup.find_all('a'):
-            if link.get('href') is not None and link.get('href')[0]!='#':
+            if link.get('href') is not None  and link.get('href') != '' and link.get('href')[0]!='#':
                 links.append(link.get('href'))
 
-        head = str(soup.title.string)
-        text = soup.get_text().encode('ascii','replace')
-
-        with open(str(count)+".txt",'w') as f:
-            f.write(text)
-        print "write successful for url", url
-        #with open("test_soup.txt",'w') as f:
-         #   f.write(soup)
-
-        links = links[:2]
-        #real_url = response.geturl()
+        #links = links[:2]
 
         for out_url in links:
-            print "Before canonicalization ", out_url
+            #print "Before canonicalization ", out_url
             new_url = canonicalize(out_url, url)
-            print "After canonicalization ", new_url
+            #print "After canonicalization ", new_url
             if front.exists(new_url):
-                print "Url exists in front (main)"
+                #print "Url exists in front (main)"
                 front.update(new_url, url)
             elif explored.get(new_url) != None:
-                print "Url doesn't exist in front (main) but exists in explored"
-                in_link_dict[new_url].append(url)
+                #print "Url doesn't exist in front (main) but exists in explored"
+                in_link_dict[new_url].add(url)
             else:
-                print "Url doesn't exist in front and doesn't exists in explored (new)"
+                #print "Url doesn't exist in front and doesn't exists in explored (new)"
                 front.push(new_url, url)
-                #in_link[new_url] = 1
 
 
             if explored.get(url) == None:
                 explored[url] = set([new_url])
-                print "new URL added in explored ",explored
+                #print "new URL added in explored ",explored
             else:
                 explored[url].add(new_url)
-                print "old URL updated in explored ",explored
+                #print "old URL updated in explored ",explored
 
         #TODO write_file(head, text, explored[url], in_links)
         #TODO update_link_graph(url, links)
-    print "Crawl for URL complete ", url
-    print "\n\n"
-    if count == 4:
-        break
-
+        #print count," Crawl for URL complete ", url
+        #print "\n\n"
+        if count == 10000:
+            break
+    else:
+        print "Skipping URL: ",url
 
 
 
--- frontier.py
+++ frontier.py
@@ -15,7 +15,7 @@ class FrontierQueue:
         for item in seed_list:
             self.seed[item] = set()
         self.front = {}
-        print "front initialized ", self.front
+        #print "front initialized ", self.front
 
     def push(self, item, in_link):
         #entry = (priority, self.count, item)
@@ -23,35 +23,39 @@ class FrontierQueue:
 
         self.front[item] = set([in_link])
         #self.count += 1
-        print "item ", item," succsessfully pushed in front", self.front
+        #print "item ", item," succsessfully pushed in front", self.front
 
     def pop(self):
         #(_, _, item) = heapq.heappop(self.heap)
+
         if len(self.seed) == 0:
             item = sorted(self.front.items(),key = lambda x:len(x[1]))[0][0]
             in_links = self.front[item]
             del self.front[item]
-            print "item ", item," successfully popped from front ", self.front
+            #print "item ", item," successfully popped from front ", self.front
             return item, in_links
         else:
             item = sorted(self.seed.items(),key = lambda x:len(x[1]))[0][0]
             in_links = self.seed[item]
             del self.seed[item]
-            print "item ", item," successfully popped from Seed ", self.seed
+            #print "item ", item," successfully popped from Seed ", self.seed
             return item, in_links
 
 
     def isEmpty(self):
-        return len(self.front) == 0
+        if len(self.front) == 0:
+            return len(self.seed) == 0
+        else:
+            return False
 
     def update(self, item, in_link):
         self.front[item].add(in_link)
-        print "item ", item," and in_link ", in_link," successfully updated in front ", self.front
+        #print "item ", item," and in_link ", in_link," successfully updated in front ", self.front
 
     def exists(self, item):
         if self.front.get(item) == None:
-            print "Item ", item," doesn't exist in Front"
+            #print "Item ", item," doesn't exist in Front"
             return False
         else:
-            print "Item ", item," exists in Front"
+            #print "Item ", item," exists in Front"
             return True
